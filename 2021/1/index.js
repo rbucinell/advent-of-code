8e94b4565b463823d30b2426e9399b45665d3296
Ryan <rbucinell.dev@gmail.com>
2021-12-01 16:16:27 -0500

    simplified files

--- 2021/1/index.js
+++ 2021/1/index.js
@@ -1,5 +1,5 @@
 const fs = require( 'fs' );
-const input = fs.readFileSync('part1-input', 'utf8').split('\n').map((num) => parseInt(num, 10));
+const input = fs.readFileSync('input', 'utf8').split('\n').map((num) => parseInt(num, 10));
 
 let counter = 0;
 input.forEach( (val, i, arr) => counter += val > arr[i-1] ? 1 : 0);
