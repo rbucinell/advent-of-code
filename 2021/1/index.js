988e935e5b3ffc2ecbd25134995f982fdcfdb7e5
Ryan <rbucinell.dev@gmail.com>
2021-12-02 15:32:32 -0500

    solved day 1 part 2

--- 2021/1/index.js
+++ 2021/1/index.js
@@ -3,4 +3,13 @@ const input = fs.readFileSync('input', 'utf8').split('\n').map((num) => parseInt
 
 let counter = 0;
 input.forEach( (val, i, arr) => counter += val > arr[i-1] ? 1 : 0);
-console.log( counter );
+console.log( `Part 1 = ${counter}` );
+
+threemeasurewindow = input.map((val, index, arr)=> {
+    if( index + 2 < arr.length) 
+        return (arr[index] + arr[index+1] + arr[index+2]);
+} );
+console.log( threemeasurewindow);
+let counter2 = 0;
+threemeasurewindow.forEach( (val, i, arr) => counter2 += val > arr[i-1] ? 1 : 0);
+console.log( `Part 2 = ${counter2}`);
