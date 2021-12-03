56feaf6c8546e090e65915b9a19c6ecdc1a7a6a2
Ryan <rbucinell.dev@gmail.com>
2021-12-03 16:18:52 -0500

    prepping for part 2

--- 2021/3/index.js
+++ 2021/3/index.js
@@ -1,13 +1,18 @@
 const fs = require( 'fs' );
 const powerConsumption = ( gamma, epsilon ) => parseInt(gamma,2) * parseInt(epsilon,2);
+const lifeSupportRating = ( oxygenGen, co2scrub ) => oxygenGen * co2scrub;
+
+const mostCommonAtBitPosition = ( input, col ) => {
+    let count1 = 0, count0 = 0;
+    input.forEach(element => element[col] === '1' ? count1++ : count0++);
+    return ( count1 > count0 ) ? '1' : '0';
+}
 
 const calculateGamma = function( input ) {
     let gammaStr = "";
     console.log( input)
     for( let col = 0; col < input[0].length; col++) {
-        let count1 = 0, count0 = 0;
-        input.forEach(element => element[col] === '1' ? count1++ : count0++);
-        gammaStr += ( count1 > count0 ) ? '1' : '0';
+        gammaStr += mostCommonAtBitPosition(input, col);
     }    
     return gammaStr;
 }
@@ -17,9 +22,12 @@ const calculateEpsilon = function( input ) {
     return gamma.split('').map( e => e === '1'? '0': '1').join('');
 }
 
-const input = fs.readFileSync('input', 'utf8').split('\n').map( e => e.trim().split('') );
+const input = fs.readFileSync('sample', 'utf8').split('\n').map( e => e.trim().split('') );
 let gamma = calculateGamma( input );
 let epsilon = calculateEpsilon( input );
+
+let oxygen = 0, co2 = 0;
+
 console.log(parseInt(gamma,2) ,parseInt(epsilon,2) )
 console.log( `γ= ${gamma} ε= ${epsilon} power= ${powerConsumption(gamma,epsilon)}`);
-
+console.log( `Life Support Rating = ${lifeSupportRating(oxygen,co2)}`)
