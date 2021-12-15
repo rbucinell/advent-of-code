e5a4b8d3a17d0120839e0a4aa0616419800c223f
Ryan <rbucinell.dev@gmail.com>
2021-12-15 16:56:24 -0500

    solved part 1

--- 2021/5/index.js
+++ 2021/5/index.js
@@ -2,13 +2,13 @@ const fs = require( 'fs' );
 const HydrothermalVent = require('./vent');
 const Diagram = require('./diagram');
 
-let input = fs.readFileSync('sample', 'utf8').split('\n');
+let input = fs.readFileSync('input', 'utf8').split('\n');
 let vents = [];
 input.forEach(element => {
     let vent = HydrothermalVent.parseVent( element );
-    console.log( `vent ${vent}`, vent.points );
     if( vent.x1 === vent.x2 || vent.y1 === vent.y2 )
         vents.push( vent );
 });
 let diagram = new Diagram( vents );
 diagram.display();
+console.log( `Part 1 Count: ${ diagram.solvePart1() }`);
