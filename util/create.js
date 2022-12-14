004015d1a88f281b390550bd0164d4d58c08f1c4
Bucinell, Ryan <rbucinell.dev@gmail.com>
2022-12-14 15:38:18 -0500

    expanding default template

--- util/create.js
+++ util/create.js
@@ -20,5 +20,19 @@ let inputData = await download(year,day);
 fs.writeFileSync(`./${year}/${day}/input.txt`, inputData);
 fs.writeFileSync(`./${year}/${day}/index.js`, `import {read} from '../../util/input.js'
 let [example,input] = ['example','input'].map( i => read( ${year}, ${day}, i ));
-console.log( input );
+console.log( example );
+
+function part1( data ){
+    
+}
+
+function part2( data ){
+    
+}
+
+console.log( "Example Part1:",part1( example ) );
+console.log( "Example Part1:",part1( input ) );
+
+console.log( "Example Part1:",part2( example ) );
+console.log( "Example Part1:",part2( input ) );
 `);
