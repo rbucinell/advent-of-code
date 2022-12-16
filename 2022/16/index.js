7110486547b0ab3f1c342f06b78cc549069dbf14
Bucinell, Ryan <rbucinell.dev@gmail.com>
2022-12-16 02:11:47 -0500

    rebuilt with new create script

--- 2022/16/index.js
+++ 2022/16/index.js
@@ -1,6 +1,7 @@
-import {read} from '../../util/input.js'
-let [example,input] = ['example','input'].map( i => read( 2022, 16, i ));
-console.log( example );
+import {readFromDir} from '../../util/input.js'
+import path from 'path'
+const curDir = path.dirname(new URL(import.meta.url).pathname);
+let [example,input] = ['example','input'].map( f => readFromDir(curDir, f));
 
 function part1( data ){
     
@@ -11,7 +12,7 @@ function part2( data ){
 }
 
 console.log( "Example Part1:",part1( example ) );
-console.log( "Example Part1:",part1( input ) );
+console.log( "Input   Part1:",part1( input ) );
 
-console.log( "Example Part1:",part2( example ) );
-console.log( "Example Part1:",part2( input ) );
+console.log( "Example Part2:",part2( example ) );
+console.log( "Input   Part2:",part2( input ) );
