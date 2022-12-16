c9a968e87a738c9719416b4f871022395f5cee1c
Bucinell, Ryan <rbucinell.dev@gmail.com>
2022-12-16 01:18:24 -0500

    stubbing out 15 & 16

---
+++ 2022/15/index.js
@@ -0,0 +1,17 @@
+import {read} from '../../util/input.js'
+let [example,input] = ['example','input'].map( i => read( 2022, 15, i ));
+console.log( example );
+
+function part1( data ){
+    
+}
+
+function part2( data ){
+    
+}
+
+console.log( "Example Part1:",part1( example ) );
+console.log( "Example Part1:",part1( input ) );
+
+console.log( "Example Part1:",part2( example ) );
+console.log( "Example Part1:",part2( input ) );
