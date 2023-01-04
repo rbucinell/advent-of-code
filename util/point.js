75f72792e6a8a4e28dbd457233116d90bfe449cf
Bucinell, Ryan <rbucinell.dev@gmail.com>
2023-01-04 16:02:11 -0500

    expanding functionality on util class

--- util/point.js
+++ util/point.js
@@ -1,6 +1,11 @@
 export default class Point{ 
-    constructor(x,y){
+    constructor(x,y,z=0){
         this.x = x;
         this.y = y;
+        this.z = z;
+    }
+
+    toString(){
+        return `(${this.x},${this.y},${this.z})`;
     }
 }
