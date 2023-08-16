9c6f8a48f5dceff0243ad96939c5bc803a902cd2
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-08-15 23:51:46 -0400

    2015/1 solved

--- 2015/1/index.js
+++ 2015/1/index.js
@@ -4,15 +4,29 @@ const curDir = path.dirname(new URL(import.meta.url).pathname);
 let [example,input] = ['example','input'].map( f => readFromDir(curDir, f));
 
 function part1( data ){
-    
+    data = data[0];
+    let floor = 0;
+    for(let i = 0; i < data.length; i++ )
+    {
+        floor += data[i] === '(' ? 1 : -1;
+    }
+    return floor;
 }
 
 function part2( data ){
-    
+    data = data[0];
+    let floor = 0;
+    let i = 0;
+    for(; i < data.length; i++ )
+    {
+        floor += data[i] === '(' ? 1 : -1;
+        if( floor === -1 ) break;
+    }
+    return i+1;
 }
 
 console.log( "Example Part1:",part1( example ) );
-console.log( "Input   Part1:",part1( input ) );
+console.log( "Input   Part1:",part1( input ) ); //280
 
 console.log( "Example Part2:",part2( example ) );
-console.log( "Input   Part2:",part2( input ) );
+console.log( "Input   Part2:",part2( input ) ); //1797
