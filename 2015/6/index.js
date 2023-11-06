268e3664f08eecb4c9253522412dc58c92e7f0f9
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-11-06 15:16:40 -0500

    solved 2015/6/2

--- 2015/6/index.js
+++ 2015/6/index.js
@@ -3,12 +3,6 @@ import path from 'path'
 const curDir = path.dirname(new URL(import.meta.url).pathname);
 let [example,input] = ['example','input'].map( f => readFromDir(curDir, f));
 
-const Actions = {
-    Toggle: 'toggle',
-    Off: 'turn off',
-    On: 'turn on'
-}
-
 class Instruction {
     constructor( action, startX, startY, endX, endY, desc ){
         this.action = action;
@@ -38,9 +32,6 @@ function parseData( data ){
     });
 
     return instructions;
-    //toggle 753,664 through 970,926
-    //turn off 150,300 through 213,740
-    //turn on 141,242 through 932,871
 }
 
 
@@ -69,19 +60,31 @@ function part1( data ){
         }
     });
     return lights.flat().reduce( (sum,cur) => sum+= cur, 0);
-
-    // for( let i = 0; i < lights.length; i++ )
-    // {
-    //     for( let j = 0; j < lights[0].length; j++ )
-    //     {
-    //         sum += lights[i][j];
-    //     }
-    // }
-    // return sum;
 }
 
 function part2( data ){
+    let instructions = parseData( data );
+    let lights = newGrid(1000, 1000 );
     
+    instructions.forEach( i => 
+    {
+        for( let x = i.start.x; x <= i.end.x; x++ )
+        {
+            for( let y = i.start.y; y <= i.end.y; y++ )
+            {
+                if( i.action === 'on' ){
+                    lights[y][x] += 1;
+                }else if( i.action === 'off' ){
+                    lights[y][x] -= 1;
+                    if( lights[y][x] < 0) 
+                        lights[y][x] = 0;
+                }else if( i.action === 'toggle'){
+                    lights[y][x] += 2;
+                }
+            }
+        }
+    });
+    return lights.flat().reduce( (sum,cur) => sum+= cur, 0);
 }
 
 console.log( "Example Part1:",part1( example ) ); //1991121 too high
