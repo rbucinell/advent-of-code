261713e5abf0fa551bd6ae569780b51287e7f910
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-12-04 13:24:36 -0500

    solved 2023/4/2

--- 2023/4/index.js
+++ 2023/4/index.js
@@ -17,22 +17,28 @@ class Card {
 
         this.winners = wins;
         this.numbers = nums;
+        this.matches = [];
+        this.numbers.forEach( n => {
+            if( this.winners.includes(n)){
+                this.matches.push( n );
+            }
+        });
+    }
 
-
-
+    winningCopies() {
+        let arr = [];
+        let len = this.matches.length;
+        for( let i = 1; i <= len; i++ ){
+            arr.push( this.id + i );
+        }
+        return arr;
     }
 
     value() {
-        let val = 0;
-        this.numbers.forEach( n => {
-            if( this.winners.includes(n) ){
-                val = val === 0 ? 1 : val * 2;
-            }
-        });
-        return val;
+        if( this.matches.length === 0) return 0;
+        return Math.pow(2, this.matches.length-1);
     }
 }
-
 
 function part1( data ){
     let cards = data.map( dr => new Card( dr ));
@@ -40,7 +46,24 @@ function part1( data ){
 }
 
 function part2( data ){
-    
+    let cards = data.map( dr => new Card( dr )).reduce( (acc,cur) => {
+        acc[cur.id] = cur;
+        return acc;
+    }, {} );
+
+    let memo = {};
+    let winners = Object.keys(cards).map( k => parseInt( k ));
+    return countWinners( winners, cards, memo );
+}
+
+function countWinners( winners, cards, memo ){
+    if( winners.length === 0) return 0;
+    let winner = winners.shift();    
+    if( !(winner in memo) ){
+        let copies = cards[winner].winningCopies();
+        memo[winner] = copies.length === 0 ? 1 : countWinners( copies, cards, memo) + 1;
+    }
+    return memo[winner] + countWinners( winners, cards, memo );
 }
 
 console.log( "Example Part1:",part1( example ) );
@@ -48,3 +71,8 @@ console.log( "Input   Part1:",part1( input ) );
 
 console.log( "Example Part2:",part2( example ) );
 console.log( "Input   Part2:",part2( input ) );
+
+//Example Part1: 13
+//Input   Part1: 21088
+//Example Part2: 30
+//Input   Part2: 6874754
