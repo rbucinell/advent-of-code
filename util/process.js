97ed3ee98bca96333497539f8b497ec5c0b6490a
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-12-05 16:28:35 -0500

    gotta make things look nice

--- util/process.js
+++ util/process.js
@@ -1,8 +1,13 @@
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export function measured( input, fn, data ){
     let start = performance.now();
     let response = fn( data );
     let end = performance.now();
-    console.log( `[${(end - start).toFixed(2)}ms]\t${input}\t${ fn.name }: ${ response} ` );
+    console.log( `[${(end - start).toFixed(2)}ms]\t${capitalize(input)}\t${ capitalize(fn.name)}: ${response} ` );
 }
 
 export function execute( functions, inputs ){
