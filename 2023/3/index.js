1d486761b96086f5e8498509d418cfdf9ce5d6ee
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-12-06 11:53:38 -0500

    finally got that mess done

--- 2023/3/index.js
+++ 2023/3/index.js
@@ -1,70 +1,111 @@
-import {readFromDir} from '../../util/input.js'
-import path from 'path'
-const curDir = path.dirname(new URL(import.meta.url).pathname);
-let [example,input] = ['example','input'].map( f => readFromDir(curDir, f));
+import { build } from '../../util/input.js';
+import { execute } from '../../util/process.js';
+let inputs = build(import.meta.url);
 
 const isNumber = ( c => /^\d+$/.test(c) );
 const symbols =  ['%', '*', '#', '&', '$', '@', '/', '=', '+', '-'];
 const isSymbol = ( s => symbols.includes(s));
 
-function hasSymbolNeighbor( row, col, grid ) {
-    for( let x = -1; x <= 1; x++ ){
-        for( let y = -1; y <= 1; y++ ){
-            if( x===0 && y===0 ) continue;
-            let r = x + row;
-            let c = y + col;
-            if( r < 0 || c < 0 ) continue;
-            if( r >= grid.length || c >= grid[r].length ) continue;
-            if( isSymbol(grid[r][c] ) )
+function hasAdjecentSymbol( data, r, c )
+{
+    for( let i = -1; i <= 1; i++ ) {
+        for( let j = -1; j <= 1; j++ ){
+            if( i === 0 && j === 0) continue;
+            let row = r + i;
+            let col = c + j;
+            if( row < 0 || col < 0 ) continue;
+            if( row >= data.length ) continue;
+            if( col >= data[row].length ) continue;
+            if( isSymbol( data[row][col] )) 
                 return true;
         }
     }
     return false;
 }
 
-function splitMulti(str, tokens){
-    let tempChar = tokens[0];
-    for(let i = 1; i < tokens.length; i++){
-        str = str.split(tokens[i]).join(tempChar);
-    }
-    str = str.split(tempChar);
-    return str;
-}
-
 function part1( data ){
-    let numbers = [];
-    for( let r = 0; r < data.length; r++) {
-        let row = data[r];
 
-        let splitRow = row
-            .split('.') //remove all '.'
-            .map( e => splitMulti( e, symbols )) //further split on all symbols if two numbers conjoined by symbol
-            .flat() // bring all array of arrays down to 1 level
-            .filter( e => e !== ''); //filter any empty strings that removed symbols caused.
-        let startIndex = 0;
-        splitRow.forEach(number => {
-            //foreach number get start and end index
-            let index = row.indexOf( number, startIndex );
-            let endIndex = index + number.length;
-            for( let i = index; i <= endIndex; i++ ){
-                let curChar = row[i];
-                if( hasSymbolNeighbor(r,i,data) ){
-                    numbers.push( number );
-                    break;
+    let partNumbers = [];
+
+    for( let r = 0; r < data.length; r++){
+
+        let curNumbers = '';
+        let neighborSymbol = false;
+
+        for( let c = 0; c < data[r].length; c++ ){
+            let char = data[r][c];
+            if( isNumber( char ) ){
+                curNumbers += char;
+                if( hasAdjecentSymbol(data,r,c) ){
+                    neighborSymbol = true;
                 }
-            }                
-            startIndex = endIndex;
-        });
+            }else{
+                if( neighborSymbol )
+                    partNumbers.push( curNumbers )
+
+                curNumbers = '';
+                neighborSymbol = false;
+            }
+        }
+        if( curNumbers && neighborSymbol ) 
+            partNumbers.push( curNumbers )
     }
-    return numbers.reduce( (acc,cur) => acc + parseInt(cur), 0);
+    return partNumbers.reduce( (acc,cur) => acc + parseInt(cur), 0);
 }
 
 function part2( data ){
-    
+    let numbers = [];
+    let gears = [];
+    for( let r = 0; r < data.length; r++){
+        let cords = [];
+        let curNumbers = '';
+        for( let c = 0; c < data[r].length; c++ ){
+            let char = data[r][c];
+            if( char === '*') 
+                gears.push( {r,c, 'adjacent': []})
+            if( isNumber( char ) ){
+                curNumbers += char;
+                cords.push({r,c})
+            }else{
+                if( curNumbers )
+                    numbers.push( {number: parseInt(curNumbers), cords } );
+                cords = [];
+                curNumbers = '';
+            }
+        }
+        if( curNumbers ) {
+            numbers.push( {number: parseInt(curNumbers), cords } );
+            cords = [];
+            curNumbers = '';
+        }
+    }
+
+    let gearRatios = [];
+    for( let g of gears ) {
+        let adjacent = [];
+        for( let n of numbers ) {
+            let numIsAdjecent = false;
+            for( let c of n.cords ){
+                if(Math.abs( g.r - c.r )<=1 && Math.abs(g.c - c.c ) <= 1)
+                {
+                    numIsAdjecent = true;
+                    break;
+                }
+            }
+            if( numIsAdjecent ){
+                adjacent.push( n.number );
+            }
+            if( adjacent.length > 2 ) break;
+        }
+        if( adjacent.length === 2)
+            gearRatios.push( adjacent[0] * adjacent[1]);
+    }
+    return gearRatios.reduce( (acc,cur) => acc + cur, 0);
 }
 
-console.log( "Example Part1:",part1( example ) );
-console.log( "Input   Part1:",part1( input ) );
+execute([part1, part2], inputs);
 
-console.log( "Example Part2:",part2( example ) );
-console.log( "Input   Part2:",part2( input ) );
+//[0.25ms]	Example	Part1: 4361 
+//[5.59ms]	Input	Part1: 527364 
+//[0.37ms]	Example	Part2: 467835 
+//[27.01ms]	Input	Part2: 79026871 
