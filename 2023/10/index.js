9700725d26f62525fe99dc8b2d226c2c0db3d1ad
Ryan Bucinell <rbucinell.dev@gmail.com>
2023-12-12 15:56:52 -0500

    solved 2023/10/1

--- 2023/10/index.js
+++ 2023/10/index.js
@@ -1,7 +1,7 @@
 import { build } from '../../util/input.js';
 import { execute } from '../../util/process.js';
 import {Grid, Node, Loc} from '../../util/grid.js';
-let inputs = build(import.meta.url, ['simple', 'example','6820-337','3022-0','7012-_','input']);// ['3022-0', 'input']);
+let inputs = build(import.meta.url, ['simple', 'example','6820-337','3022-0','7012-_','input']);
 
 class TileGrid extends Grid{
     getNeighbors(node){
@@ -16,12 +16,36 @@ class TileGrid extends Grid{
             case 'J': restrictions = [ loc.to(-1,0), loc.to(0,-1)];break;
             case '7': restrictions = [ loc.to(1,0),  loc.to(0,-1)];break;
             case 'F': restrictions = [ loc.to(1,0),  loc.to(0,1) ];break;
-            case 'S': restrictions = neighbors; break;
+            case 'S': 
+                restrictions = neighbors.filter( neighbor => {
+                    let node = this.getNode(neighbor.r, neighbor.c);
+                    let neighborOfNeighbors = this.getNeighbors(node);
+                    return neighborOfNeighbors.some( non => non.value === 'S');
+                });
+                break;
             default: restrictions = [];  break;
         }
         //Now filter on the problem specific restrictions
         neighbors = neighbors.filter( n => restrictions.some( r => n.equals(r) ) );
-        return neighbors.map( neighbor => this.getNode( neighbor.r, neighbor.c));
+        neighbors = neighbors.map( neighbor => this.getNode( neighbor.r, neighbor.c));
+        neighbors = neighbors.filter( n => n.value !== '.' );
+        return neighbors; 
+    }
+
+    displayWindow( r, c ){
+        console.log( '         ');
+        for( let i = -1; i <= 1; i++ ){
+            let row = [];
+            for( let j = -1; j<= 1; j++ ){
+                let outsym = ' ';
+                try{
+                    outsym = this.getNode(r+i, c+j).value;
+                }catch{ outsym = 'X'};
+                row.push( ` ${outsym} ` );
+            }
+            console.log( row.join('') );
+        }
+        console.log('---------')
     }
 
     display(){
@@ -53,7 +77,21 @@ class TileGrid extends Grid{
 function part1( data ){
     let grid = new TileGrid(data.length, data[0].length, data.map( row => row.split('')));
     let start = grid.find('S');
+    // let queue = [grid.getNeighbors(start)[0]];
+    // let visited = [start];
+    // let count = 1;
+    // while( queue.length > 0 ){
+    //     let cur = queue.shift();
+    //     visited.push( cur );
+    //     count++;
+    //     let neighbors = grid.getNeighbors(cur);
+    //     neighbors = neighbors.filter( n => !visited.includes( n ));
+    //     if( neighbors.length > 0)
+    //         queue.push( neighbors?.shift() );
+    // }
+    // return count /2 ;
 
+    //bfs approach
     let visited = [];
     start.dist = 0;
     let queue = [start];
@@ -61,6 +99,7 @@ function part1( data ){
     while( queue.length > 0 )
     {
         let cur = queue.shift();
+        //grid.displayWindow(cur.loc.r, cur.loc.c);
         visited.push( cur );
         let neighbors = grid.getNeighbors(cur);
         for( let neighbor of neighbors ) {
@@ -80,7 +119,27 @@ function part1( data ){
 }
 //6735 too low
 function part2( data ){
-    return 0;
+    let visited = [];
+    start.dist = 0;
+    let queue = [start];
+    let dist = start.dist;
+    while( queue.length > 0 )
+    {
+        let cur = queue.shift();
+        //grid.displayWindow(cur.loc.r, cur.loc.c);
+        visited.push( cur );
+        let neighbors = grid.getNeighbors(cur);
+        for( let neighbor of neighbors ) {
+            if( !('dist' in neighbor)) {
+                neighbor.dist = cur.dist + 1;
+                dist = Math.max( dist, neighbor.dist );
+            }
+            if( !visited.includes( neighbor ) )
+            {
+                queue.push( neighbor );
+            }
+        }
+    }
 }
 
 execute([part1, part2], inputs);
