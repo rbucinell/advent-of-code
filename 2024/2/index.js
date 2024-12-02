74aec2aa64d6c685906c7d511ac5f9ef4e42bf9c
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-12-02 08:11:28 -0500

    solved 2024/2/2

--- 2024/2/index.js
+++ 2024/2/index.js
@@ -20,45 +20,62 @@ class Report {
         return true;
     }
 
-
-    dampenedSafe(){
-        let arr = JSON.parse(JSON.stringify(this.levels));
-        let dampUsed = false;
+    isArrSafe( arr, dampened = false ){
+        let isSafe = true;
+        let dampUsed = dampened;
         const sign = (arr[0]-arr[1])/Math.abs(arr[0]-arr[1]);
         
         for( let i = 0; i < arr.length-1; i++){
             let diff = arr[i]-arr[i+1];
             let abs = Math.abs(diff);
-            console.log( arr[i], arr[i+1], `[${arr.join(',')}]`, diff, sign !== diff/abs );
+            //console.log( arr[i], arr[i+1], `[${arr.join(',')}]`, diff, sign !== diff/abs );
             if( sign !== diff/abs || abs < 1 || abs > 3){
                 if( dampUsed ){
-                    console.log( `Report is safe`)
-                    return false;
+                    //console.log( `Report is safe`)
+                    isSafe = false;
+                    break;
                 }else{
                     dampUsed = true;
-                    console.log( `Removing ${arr[i]}`)
+                    //console.log( `Removing ${arr[i]}`)
                     arr.splice(i,1);
-                    console.log( `[${arr.join(',')}]`)
+                    //console.log( `[${arr.join(',')}]`)
                     i-=2;
                 }
             }
         }
-        console.log( `Report is safe`)
-        return true;
+        //console.log( `Report is safe`)
+        return isSafe;
     }
-    
 
+    dampenedSafe(){
+
+        let arr = JSON.parse( JSON.stringify(this.levels) ).map( _ => parseInt( _ ));
+        if( this.isArrSafe(arr)) return true;
+
+        for( let i = 0; i < this.levels.length; i++ ) {
+            arr = JSON.parse( JSON.stringify(this.levels) ).map( _ => parseInt( _ ));
+            arr.splice( i, 1 );
+            if( this.isArrSafe( arr, true )) return true;
+        }
+        return false;
+    }
 }
 
 function part1( data ){
-    let reports = data.map( r => new Report( r.split(' ') ));
+    let reports = data.map( r => new Report( r.split(' ').map( r => parseInt(r)) ));
     return reports.filter( r => r.isSafe() ).length;
 }
 
 function part2( data ){
-    let reports = data.map( r => new Report( r.split(' ') ));
+    let reports = data.map( r => new Report( r.split(' ').map( r => parseInt(r)) ));
     return reports.filter( r => r.dampenedSafe() ).length;
-    //Answer not 417,437,491
 }
+
+/*
+[0.17ms]	Part1	Example: 2
+[1.35ms]	Part1	Input: 407
+[0.20ms]	Part2	Example: 4
+[6.13ms]	Part2	Input: 459
+*/
 
 execute([part1, part2], inputs);
