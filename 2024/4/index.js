610dd164842527d2f9478418c59872610657263c
Ryan Bucinell <rbucinell.dev@gmail.com>
2024-12-04 00:30:08 -0500

    solved 2024/4/1

---
+++ 2024/4/index.js
@@ -0,0 +1,69 @@
+import { build } from '../../util/input.js';
+import { execute } from '../../util/process.js';
+let inputs = build(import.meta.url);
+
+function findXMASat( data, x, y ){
+
+    //console.log( "(x,y): ",x, y );
+    let findCount = 0;
+
+    let directions = [
+        [{ x: 0,  y: -1 }, {  x: 0,  y: -2 },  { x: 0,  y: -3 }], //N
+        [{ x: 1,  y: -1 }, {  x: 2,  y: -2 },  { x: 3,  y: -3 }], //NE
+        [{ x: 1,  y: 0 },  {  x: 2,  y: 0  },  { x: 3,  y: 0 }],  //E
+        [{ x: 1,  y: 1 },  {  x: 2,  y: 2  },  { x: 3,  y: 3 }],  //SE
+        [{ x: 0,  y: 1 },  {  x: 0,  y: 2  },  { x: 0,  y: 3 }],  //S
+        [{ x: -1, y: 1 },  {  x: -2, y: 2  },  { x: -3, y: 3 }],  //SW
+        [{ x: -1, y: 0 },  {  x: -2, y: 0  },  { x: -3, y: 0 }],  //W
+        [{ x: -1, y: -1 },  { x: -2, y: -2 },  { x: -3, y: -3 }], //SW
+    ];
+
+    const MAS = "MAS";
+    for( let dir of directions ){
+
+        //console.log( "DIR: ", JSON.stringify(dir));
+        let found = true;
+        for(let i = 0; i < MAS.length; i++ ) {
+            let letter = MAS.charAt(i);
+            let d = dir[i];
+
+            let cord = {
+                x: x + d.x,
+                y: y + d.y
+            }
+            //console.log( "Letter", MAS.charAt(i),"Cord:", JSON.stringify(cord ));
+            if( cord.x < 0 || cord.y < 0 || cord.x >= data.length || cord.y >= data[cord.x].length ){
+                found = false;
+                break;
+            }
+
+            if( data[cord.x][cord.y] !== letter ){
+                found = false;
+                break;
+            }
+        }
+        if( found ) findCount++;
+
+    }
+
+    return findCount;
+}
+
+
+function part1( data ){
+    let sum = 0;
+    for( let i = 0; i < data.length; i++ ){
+        for( let j = 0; j < data[i].length; j++ ){
+            if( data[i][j] === 'X'){
+                sum += findXMASat(data, i, j );
+            }
+        }
+    }
+    return sum;
+}
+
+function part2( data ){
+    return 0;
+}
+
+execute([part1, part2], inputs);
