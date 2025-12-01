72b36209c265dd1b08b6ebdbc4938aa4bf10c542
Ryan Bucinell <rbucinell.dev@gmail.com>
2025-12-01 18:41:08 -0500

    solved 2025-1-2

--- 2025/1/index.js
+++ 2025/1/index.js
@@ -26,9 +26,36 @@ function part1( data ){
     return password;
 }
 
+//0x434C49434B  => CLICK
 function part2( data ){
-    return 0;
+    let position = INITAIL_POSITION;
+    let password = 0;
+    data.forEach(rotation => {
+        let dir = rotation.substring(0,1) === 'R' ? 1 : -1;
+        let dist = parseInt(rotation.substring(1));
+        let crossed = 0;
+        for( let i = 0; i < dist; i++ ){
+            position += dir;
+            
+            if( dir > 0 && position === 100 ){
+                position = 0;
+            } 
+            
+            if( dir < 0 && position === -1 ) {
+                position = 99;
+            }
+            if( position === 0 ){
+                crossed++;
+            }
+        }
+        password += crossed;
+    });
+
+    return password;
 }
 
-//Part1: 1165
 execute([part1, part2], inputs);
+// [0.32ms]	Part1	Example: 3 
+// [12.74ms]	Part1	Input: 1165 
+// [0.49ms]	Part2	Example: 6 
+// [12.21ms]	Part2	Input: 6496 
