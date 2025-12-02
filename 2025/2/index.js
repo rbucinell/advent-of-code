b761e8b259f50dfd8a979b80321751e0e2a83924
Ryan Bucinell <rbucinell.dev@gmail.com>
2025-12-02 00:47:49 -0500

    initi 2025-02

---
+++ 2025/2/index.js
@@ -0,0 +1,13 @@
+import { build } from '../../util/input.js';
+import { execute } from '../../util/process.js';
+let inputs = build(import.meta.url);
+
+function part1( data ){
+    return 0;
+}
+
+function part2( data ){
+    return 0;
+}
+
+execute([part1, part2], inputs);
